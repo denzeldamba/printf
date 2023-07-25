6eac7a7f0e3c6c019c12254ba5ce659fbeeaba41
denzeldamba <denzeldamba42@gmail.com>
2023-07-26 00:00:48 +0300

    Education is when you read the fine print. Experience is what you get if you don't

---
+++ int.c
@@ -0,0 +1,39 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars = 0;
+	va_list arguments;
+
+	va_start(arguments, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			switch (*format)
+			{
+				case 'i':
+					printed_chars += printf("%i", va_arg(arguments, int));
+					break;
+				case 'd':
+					printed_chars += printf("%d", va_arg(arguments, char *));
+					break;
+			}
+		}
+		format++;
+	}
+
+	va_end(arguments);
+	return (printed_chars);
+}
