584d6b145d926b8026e3b1d9ad462b0bbbcbe850
denzeldamba <denzeldamba42@gmail.com>
2023-07-26 00:03:29 +0300

    Education is when you read the fine print. Experience is what you get if you don't

--- int.c
+++ int.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 					printed_chars += printf("%i", va_arg(arguments, int));
 					break;
 				case 'd':
-					printed_chars += printf("%d", va_arg(arguments, char *));
+					printed_chars += printf("%d", va_arg(arguments, int));
 					break;
 			}
 		}
